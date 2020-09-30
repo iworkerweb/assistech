90c54a9b44f3b77470d116a2d3a518ec688db0ee
wangdong <dong.wang@gongkong.com>
2020-09-30 19:34:29 +0800

    Button组件的添加

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
-
+import Button from './components/Button/button'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button btnType="primary">按钮</Button>
     </div>
   );
 }
